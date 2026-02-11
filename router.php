9ab369bf09b453230f0d2603ea87bc005b193d06
ACLL <120932801+3POPSTA@users.noreply.github.com>
2026-02-11 16:11:46 +0000

    Add files via upload

---
+++ router.php
@@ -0,0 +1,26 @@
+<?php 
+
+    $url =parse_url($_SERVER["REQUEST_URI"])["path"];
+    $routes = [
+        "/" => "controllers/index.php",
+        "/about" => "controllers/about.php",
+        "/contact" => "controllers/contact.php",
+
+    ];
+    function routing($url,$routes){
+        if(array_key_exists($url,$routes)){
+            require $routes[$url];
+        }
+        else{
+            abort();
+        }
+    }
+    function abort($code = 404){
+        http_response_code($code);
+        require "{$code}.php";
+        die();
+
+    }
+    routing($url,$routes);
+
+?>
